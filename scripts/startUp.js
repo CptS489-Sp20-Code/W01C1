c5fa361293df2e1d44527bfdcb10111a0f0f0c53
Chris Hundhausen <chundhau@gmail.com>
2019-11-11 16:45:08 -0800

    Tidied up for presentation

---
+++ scripts/startUp.js
@@ -0,0 +1,7 @@
+//startUp.js -- This file contains JavaScript code that is executed when the app first launches
+includeHTML();
+//The following code hides all pages except for the login page.
+document.getElementById("profileDiv").style.display = "none";
+document.getElementById("roundsDiv").style.display = "none";
+document.getElementById("coursesDiv").style.display = "none";
+document.getElementById("logRoundDiv").style.display = "none";
